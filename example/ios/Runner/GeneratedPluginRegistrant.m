d9b2ceae4b4ed2c6cf40936b40b5b11e6b757f87
LG - Laptop <ngocson160391@gmail.com>
2020-10-26 16:46:46 +0700

    commit miss file

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_zalo_sdk/FlutterZaloSdkPlugin.h>)
+#import <flutter_zalo_sdk/FlutterZaloSdkPlugin.h>
+#else
+@import flutter_zalo_sdk;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterZaloSdkPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterZaloSdkPlugin"]];
+}
+
+@end
