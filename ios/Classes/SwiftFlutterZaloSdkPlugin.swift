e6ba176ed2710aaeee1a488391f157f1e75185a7
LG - Laptop <ngocson160391@gmail.com>
2020-10-26 16:29:52 +0700

    initial commit

--- ios/Classes/SwiftFlutterZaloSdkPlugin.swift
+++ ios/Classes/SwiftFlutterZaloSdkPlugin.swift
@@ -1,7 +1,8 @@
 import Flutter
 import UIKit
+import 'ZaloPaySDK.h'
 
-public class SwiftFlutterZaloSdkPlugin: NSObject, FlutterPlugin {
+public class SwiftFlutterZaloSdkPlugin: NSObject, FlutterPlugin , ZPPaymentDelegate {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "flutter_zalo_sdk", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterZaloSdkPlugin()
@@ -10,5 +11,33 @@ public class SwiftFlutterZaloSdkPlugin: NSObject, FlutterPlugin {
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     result("iOS " + UIDevice.current.systemVersion)
+    if(call.method=="payOrder"){
+        let args = call.arguments as? [String: Any]
+        let  _zptoken = args?["zptoken"] as? String
+        ZaloPaySDK.sharedInstance()?.paymentDelegate = self
+        ZaloPaySDK.sharedInstance()?.payOrder(_zptoken)
+        result("Processing...")
+    }
   }
+
+    func paymentDidSucceeded(_ transactionId: String!, zpTranstoken: String!, appTransId: String!) {
+        //Handle Success
+        guard let eventSink = eventSink else {
+          return
+        }
+        eventSink(["errorCode": PAYMENTCOMPLETE, "zpTranstoken": zpTranstoken ?? "", "transactionId": transactionId ?? "", "appTransId": appTransId ?? ""])
+    }
+    func paymentDidCanceled(_ zpTranstoken: String!, appTransId: String!) {
+        //Handle Canceled
+        guard let eventSink = eventSink else {
+          return
+        }
+        eventSink(["errorCode": PAYMENTCANCELED, "zpTranstoken": zpTranstoken ?? "", "appTransId": appTransId ?? ""])
+    }
+    func paymentDidError(_ errorCode: ZPPaymentErrorCode, zpTranstoken: String!, appTransId: String!) {
+        guard let eventSink = eventSink else {
+          return
+        }
+        eventSink(["errorCode": PAYMENTERROR, "zpTranstoken": zpTranstoken ?? "", "appTransId": appTransId ?? ""])
+    }
 }
