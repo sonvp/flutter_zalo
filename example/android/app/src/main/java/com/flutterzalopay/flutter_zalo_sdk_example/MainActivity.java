1e0e570f23e695230056439026b6ad11fd2151c5
sonvo <ngocson160391@gmail.com>
2022-05-21 12:56:41 +0700

    fix can't build example in mac m1

--- example/android/app/src/main/java/com/flutterzalopay/flutter_zalo_sdk_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.flutterzalopay.flutter_zalo_sdk_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -4,17 +4,16 @@ import zpdk
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-    
-    override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        return ZaloPaySDK.sharedInstance().application(app, open:url, sourceApplication: "vn.com.vng.zalopay", annotation:nil)
-    }
-    
-    override func application(
+  override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        GeneratedPluginRegistrant.register(with: self)
-        ZaloPaySDK.sharedInstance()?.initWithAppId(2553, uriScheme: "demozpdk://app", environment: .sandbox)
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    ZaloPaySDK.sharedInstance()?.initWithAppId(2553, uriScheme: "demozpdk://app", environment: .sandbox)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+          return ZaloPaySDK.sharedInstance().application(app, open:url, sourceApplication: "vn.com.vng.zalopay", annotation:nil)
+      }
 }
