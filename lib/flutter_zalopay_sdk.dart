8935760f5f49a4ed508506bdfa0ecb13d4de5efa
Ha Tran <tranvietha.2809@gmail.com>
2023-08-27 23:06:36 +0700

    fix syntax error

--- lib/flutter_zalopay_sdk.dart
+++ lib/flutter_zalopay_sdk.dart
@@ -10,18 +10,17 @@ class FlutterZaloPaySdk {
       const MethodChannel('flutter.native/channelPayOrder');
 
   static Future<FlutterZaloPayStatus> payOrder({required String zpToken}) async {
-        final String result =
-        await _channel.invokeMethod('payOrder', {"zptoken": zpToken});
-        switch(result) {
-          case "User hủy thanh toán":
-            return FlutterZaloPayStatus.cancelled;
-          case "Thanh Toán Thành Công":
-            return FlutterZaloPayStatus.success;
-          case "Giao dịch thất bại":
-            return FlutterZaloPayStatus.failed;
-          default:
-            return FlutterZaloPayStatus.failed;
-        }
-      }
+    final String result =
+    await _channel.invokeMethod('payOrder', {"zptoken": zpToken});
+    switch(result) {
+      case "User hủy thanh toán":
+        return FlutterZaloPayStatus.cancelled;
+      case "Thanh Toán Thành Công":
+        return FlutterZaloPayStatus.success;
+      case "Giao dịch thất bại":
+        return FlutterZaloPayStatus.failed;
+      default:
+        return FlutterZaloPayStatus.failed;
+    }
   }
 }
